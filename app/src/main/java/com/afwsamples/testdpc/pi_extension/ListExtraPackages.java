c1f164f7723991ce21b19a859bca8972a113ff57
Evgeny Chormonov <en.chormonov@gmail.com>
2018-04-18 01:59:11 +0300

    FEAT: add list extra package for hide and remove

---
+++ app/src/main/java/com/afwsamples/testdpc/pi_extension/ListExtraPackages.java
@@ -0,0 +1,74 @@
+package com.afwsamples.testdpc.pi_extension;
+
+/**
+ * Created by ser on 08.04.18.
+ */
+
+public class ListExtraPackages {
+    public  static final  String[] DEFAULT_HIDE_PACKAGES = {
+            "com.android.calendar",
+            "com.android.chrome",
+            "com.android.contacts",
+            "com.android.contacts",
+            "com.android.deskclock",
+            "com.android.email",
+            "com.android.gallery3d",
+            "com.android.mediacenter",
+            "com.android.mms",
+            "com.android.settings",
+            "com.android.vending",
+            "com.example.android.notepad",
+            "com.google.android.apps.docs",
+            "com.google.android.apps.photos",
+            "com.google.android.gm",
+            "com.google.android.music",
+            "com.google.android.videos",
+            "com.huawei.camera",
+            "com.huawei.hidisk",
+            "com.android.calculator2",
+            "com.android.documentsui",
+            "com.android.soundrecorder",
+            "com.android.stk",
+            "com.android.systemui",
+            "com.google.android.apps.maps",
+            "com.google.android.apps.tachyon",
+            "com.google.android.googlequicksearchbox",
+            "com.google.android.googlequicksearchbox",
+            "com.huawei.android.hwouc",
+            "com.huawei.android.thememanager",
+            "com.huawei.android.totemweather",
+            "com.huawei.himovie",
+            "com.huawei.systemmanager",
+            "com.touchtype.swiftkey",
+            "com.google.android.youtube"
+    };
+
+    public  static String[] getDefaultHidePackages(){
+        return DEFAULT_HIDE_PACKAGES;
+    }
+
+    public  static final  String[] DEFAULT_REMOVE_PACKAGES = {
+            "com.huawei.kidsmode.kidspaint",
+            "com.huawei.kidsmode",
+            "com.microsoft.office.excel",
+            "com.huawei.KoBackup",
+            "com.microsoft.office.outlook",
+            "com.google.android.apps.plus",
+            "com.microsoft.office.word",
+            "com.opera.browser",
+            "com.microsoft.office.powerpoint",
+            "ru.yandex.searchplugin",
+            "com.facebook.katana",
+            "com.facebook.orca",
+            "com.google.android.apps.books",
+            "com.booking",
+            "com.huawei.android.ApplicationForCustomize",
+            "com.opera.preinstall",
+            "com.google.android.play.games",
+            "com.google.android.apps.magazines",
+    };
+
+    public  static String[] getDefaultRemovePackages(){
+        return DEFAULT_REMOVE_PACKAGES;
+    }
+}
